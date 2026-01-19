8ba4f5ac937b8262c9558015277cddc5c3c7f0ef
gmlnws3628@gmail.com <gmlnws3628@gmail.com>
2026-01-19 12:21:34 +0900

    매칭 조건 추가, log paging 및 visitorlog 수정

--- src/main/java/kr/co/ictedu/someta/chart/VisitorLogDao.java
+++ src/main/java/kr/co/ictedu/someta/chart/VisitorLogDao.java
@@ -8,9 +8,9 @@ import kr.co.ictedu.someta.vo.VisitorLogVO;
 @Mapper
 public interface VisitorLogDao {
 	@Insert("INSERT INTO VISITORSLOG(\r\n"
-			+ "    NUM, IDN, REIP, UAGENT, REQ_URI, REQ_QUERY, REFERER, METHOD, STATUS_CODE, SESSION_ID, SSTIME\r\n"
-			+ "  ) VALUES (\r\n"
-			+ "    VISITORSLOG_SEQ.NEXTVAL, #{idn}, #{reip}, #{uagent}, #{reqUri}, #{reqQuery}, #{referer}, #{method}, #{statusCode}, #{sessionId}, SYSDATE\r\n"
-			+ "  )")
-	public void insertVisitorLog(VisitorLogVO vo);
+            + "    NUM, IDN, REIP, UAGENT, REQ_URI, REQ_QUERY, REFERER, METHOD, STATUS_CODE, SESSION_ID, SSTIME\r\n"
+            + "  ) VALUES (\r\n"
+            + "    VISITORSLOG_SEQ.NEXTVAL, #{idn}, #{reip}, #{uagent}, #{reqUri}, #{reqQuery, jdbcType=VARCHAR}, #{referer, jdbcType=VARCHAR}, #{method, jdbcType=VARCHAR}, #{statusCode}, #{sessionId, jdbcType=VARCHAR}, SYSDATE\r\n"
+            + "  )")
+    public void insertVisitorLog(VisitorLogVO vo);
 }
